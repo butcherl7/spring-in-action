fbb37897ab91c2f7dfb0504ad721a0fb17bc09ea
kinglyq <755365600@qq.com>
2023-12-18 11:47:56 +0800

    remove AUTHENTICATED&PRINCIPALS

--- src/main/java/top/funsite/spring/action/shiro/RedisSubjectDAO.java
+++ src/main/java/top/funsite/spring/action/shiro/RedisSubjectDAO.java
@@ -2,21 +2,15 @@ package top.funsite.spring.action.shiro;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.mgt.DefaultSubjectDAO;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
-import org.apache.shiro.subject.support.DefaultSubjectContext;
 
 @Slf4j
 public class RedisSubjectDAO extends DefaultSubjectDAO {
 
     @Override
-    protected void mergePrincipals(Subject subject) {
-        super.mergePrincipals(subject);
-        Session session = subject.getSession();
-        if (session != null) {
-            // TODO 暂时这么做
-            session.removeAttribute(DefaultSubjectContext.AUTHENTICATED_SESSION_KEY);
-            session.removeAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY);
-        }
+    protected void saveToSession(Subject subject) {
+        super.saveToSession(subject);
+        // TODO 暂时这么做
+        super.removeFromSession(subject);
     }
 }

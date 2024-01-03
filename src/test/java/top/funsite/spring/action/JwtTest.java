d3a1671980670dbf1930a6dd310de384f501dcec
kinglyq <755365600@qq.com>
2024-01-03 18:33:12 +0800

    payload (defined)

--- src/test/java/top/funsite/spring/action/JwtTest.java
+++ src/test/java/top/funsite/spring/action/JwtTest.java
@@ -3,44 +3,81 @@ package top.funsite.spring.action;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.IncorrectClaimException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.UUID;
 
 @Slf4j
 public class JwtTest {
 
+    /**
+     * <ul>
+     *     <li>iss (issuer)：签发人</li>
+     *     <li>sub (subject)：主题</li>
+     *     <li>aud (audience)：受众</li>
+     *     <li>iat (Issued At)：签发时间</li>
+     *     <li>jti (JWT ID)：提供唯一的标识符</li>
+     *     <li>kid (Key Id)：所有权证明密钥的密钥 ID 表示</li>
+     *     <li>exp (expiration time)：过期时间</li>
+     *     <li>nbf (Not Before)：生效时间，在此之前是无效的</li>
+     * </ul>
+     */
     @Test
+    @DisplayName("测试生成 JWT")
     public void testCreate() {
+        LocalDateTime now = LocalDateTime.now();
         String sign = JWT.create()
-                // .withHeader("{\"what\": \"if\"}")
-                .withClaim("name", "Tom")
-                .withClaim("age", 18)
-                .withExpiresAt(LocalDateTime.now().plusSeconds(1).atZone(ZoneId.systemDefault()).toInstant())
+                .withIssuer("Umbrella")
+                .withSubject("Test")
+                .withAudience("a", "b", "c")
+                .withIssuedAt(new Date())
+                .withIssuedAt(new Date())
+                .withJWTId(UUID.randomUUID().toString().replaceAll("-", ""))
+                .withKeyId("Hello")
+                // .withNotBefore(now.plusMinutes(30).atZone(ZoneId.systemDefault()).toInstant())
+                .withExpiresAt(now.plusHours(1).atZone(ZoneId.systemDefault()).toInstant())
                 .sign(Algorithm.HMAC256("123456"));
-        System.out.println("\n" + sign);
+        System.out.println("\n" + sign + "\n");
     }
 
     @Test
+    @DisplayName("测试验证 JWT")
     public void testVerify() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVG9tIiwiYWdlIjoxOSwiZXhwIjoxNzAzMjE4ODQzfQ.IhaxbooB_ZAbH_ZGgqiGpi-PZkyDvL7FJUfWT_Ro3HQ1";
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVbWJyZWxsYSIsInN1YiI6IlRlc3QiLCJhdWQiOlsiYSIsImIiLCJjIl0sImlhdCI6MTcwNDI3NzUwMiwiZXhwIjoxNzA0MjgxMTAyfQ.i_3NjRVnvPm5N_hd5PF1RygQuEYaTkT8kuvMiiyypl4";
 
         try {
             JWTVerifier verifier = JWT.require(Algorithm.HMAC256("123456"))
                     .build();
-            verifier.verify(token);
+            DecodedJWT decodedJWT = verifier.verify(token);
+            String id = decodedJWT.getId();
+            String keyId = decodedJWT.getKeyId();
+            System.out.println(id);
+            System.out.println(keyId);
         } catch (JWTVerificationException e) {
             if (e instanceof TokenExpiredException) {
-                ZonedDateTime zonedDateTime = ((TokenExpiredException) e).getExpiredOn().atZone(ZoneId.systemDefault());
-                System.out.println("The Token has expired on " + zonedDateTime.format(formatter));
+                TokenExpiredException ex = (TokenExpiredException) e;
+                ZonedDateTime zonedDateTime = ex.getExpiredOn().atZone(ZoneId.systemDefault());
+                System.err.println("The Token has expired on " + zonedDateTime.format(formatter));
+            } else if (e instanceof IncorrectClaimException) {
+                IncorrectClaimException ex = (IncorrectClaimException) e;
+                String claimName = ex.getClaimName();
+                Claim claimValue = ex.getClaimValue();
+                // RegisteredClaims.NOT_BEFORE
+                System.err.printf("%s %s %s\n", claimName, claimValue, ex.getMessage());
             } else {
                 e.printStackTrace();
             }

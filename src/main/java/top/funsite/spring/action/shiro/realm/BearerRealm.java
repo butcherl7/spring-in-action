2fc12da72f4f97dd77119157b2b7f267afdd3e20
kinglyq <755365600@qq.com>
2024-01-04 19:13:25 +0800

    BearerRealm Multi test

---
+++ src/main/java/top/funsite/spring/action/shiro/realm/BearerRealm.java
@@ -0,0 +1,36 @@
+package top.funsite.spring.action.shiro.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.pam.AuthenticationStrategy;
+import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.Collection;
+
+/**
+ * 多 Realm 测试（不同的用户类型使用不同的 Realm），可以通过 {@link #supports(AuthenticationToken)}. 或者使用 {@link AuthenticationStrategy}.
+ *
+ * @see ModularRealmAuthenticator#doMultiRealmAuthentication(Collection, AuthenticationToken)
+ */
+public class BearerRealm extends AuthorizingRealm {
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authToken) throws AuthenticationException {
+        BearerToken uToken = (BearerToken) authToken;
+        String token = uToken.getToken();
+        return new SimpleAuthenticationInfo(token, token, getName());
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return new SimpleAuthorizationInfo();
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof BearerToken;
+    }
+}

d4fcba819f3ced3b2579140fda898f5abfaeb114
kinglyq <755365600@qq.com>
2024-01-09 14:02:22 +0800

    MapperScan

--- src/main/java/top/funsite/spring/action/SpringInActionApplication.java
+++ src/main/java/top/funsite/spring/action/SpringInActionApplication.java
@@ -1,13 +1,15 @@
 package top.funsite.spring.action;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("top.funsite.spring.action.mapper")
 public class SpringInActionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringInActionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInActionApplication.class, args);
+    }
 
 }

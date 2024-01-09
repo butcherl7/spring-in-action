d408142328916aef770715dc01d2bc78f6f7266f
kinglyq <755365600@qq.com>
2024-01-09 18:13:12 +0800

    fk

--- src/main/resources/sql/table.sql
+++ src/main/resources/sql/table.sql
@@ -17,24 +17,24 @@ create table sys_user
 
 create table sys_role
 (
-    name varchar_ignorecase not null unique primary key,
+    name         varchar_ignorecase not null unique primary key,
     enabled      boolean   default true,
     created_time timestamp default current_timestamp()
 );
 
 create table sys_permission
 (
-    name varchar_ignorecase not null unique primary key,
+    name         varchar_ignorecase not null unique primary key,
     enabled      boolean   default true,
     created_time timestamp default current_timestamp()
 );
 
 create table sys_user_role
 (
-    uid bigint not null
+    uid          bigint  not null
         constraint fk_ur_un references sys_user (id) on delete cascade on update cascade,
     role_name    varchar not null
-        constraint ur_ur_rn references sys_role (name) on delete cascade on update cascade,
+        constraint fk_ur_rn references sys_role (name) on delete cascade on update cascade,
     created_time timestamp default current_timestamp()
 );
 

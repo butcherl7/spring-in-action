d2a28aac032bbe392498e98eafd6246c77ced579
kinglyq <755365600@qq.com>
2024-01-09 13:58:10 +0800

    modify structure

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,12 +1,25 @@
 insert into sys_user(username, password)
 values ('admin', '123456'),
-       ('tom', '123456');
+       ('tom', '123456'),
+       ('alice', '123456');
 
-insert into sys_role(name)
-values ('scholar'),
-       ('farmer'),
-       ('artisan'),
-       ('merchant');
+insert into sys_role(name, enabled)
+values ('admin', true),
+       ('people', true);
 
-insert into sys_user_role(username, role_name)
-values ('tom', 'scholar');
+insert into sys_permission(name, enabled)
+values ('book:read:*', true),
+       ('book:borrow:*', true),
+       ('book:read:home', true),
+       ('book:borrow:home', true);
+
+insert into sys_user_role(uid, role_name)
+values (1, 'admin'),
+       (1, 'people'),
+       (2, 'people');
+
+insert into sys_role_permission (role_name, permission_name)
+values ('admin', 'book:read:*'),
+       ('admin', 'book:borrow:*'),
+       ('people', 'book:read:home'),
+       ('people', 'book:borrow:home');
--- src/main/resources/sql/table.sql
+++ src/main/resources/sql/table.sql
@@ -1,15 +1,16 @@
+drop table if exists sys_role_permission;
+drop table if exists sys_user_role;
 drop table if exists sys_user;
 drop table if exists sys_role;
 drop table if exists sys_permission;
-drop table if exists sys_user;
-drop table if exists sys_role_permission;
 
 create table sys_user
 (
-    username      varchar not null unique primary key,
+    id          bigint primary key auto_increment,
+    username    varchar not null unique,
     password      varchar not null,
     locked        boolean,
-    lockedTime    timestamp,
+    locked_time timestamp,
     enabled       boolean   default true,
     created_time  timestamp default current_timestamp(),
     modified_time timestamp default current_timestamp()
@@ -17,22 +18,22 @@ create table sys_user
 
 create table sys_role
 (
-    name         varchar not null unique primary key,
+    name varchar_ignorecase not null unique primary key,
     enabled      boolean   default true,
     created_time timestamp default current_timestamp()
 );
 
 create table sys_permission
 (
-    name         varchar not null unique primary key,
+    name varchar_ignorecase not null unique primary key,
     enabled      boolean   default true,
     created_time timestamp default current_timestamp()
 );
 
 create table sys_user_role
 (
-    username     varchar not null
-        constraint fk_ur_un references sys_user (username) on delete cascade on update cascade,
+    uid bigint not null
+        constraint fk_ur_un references sys_user (id) on delete cascade on update cascade,
     role_name    varchar not null
         constraint ur_ur_rn references sys_role (name) on delete cascade on update cascade,
     created_time timestamp default current_timestamp()

7a2c7305a5c2f9503a3e345885abca99124e7b3f
kinglyq <755365600@qq.com>
2024-01-11 17:21:13 +0800

    assert time test

--- src/main/java/top/funsite/spring/action/shiro/filter/PassThruFilter.java
+++ src/main/java/top/funsite/spring/action/shiro/filter/PassThruFilter.java
@@ -1,11 +1,15 @@
 package top.funsite.spring.action.shiro.filter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.web.filter.authc.PassThruAuthenticationFilter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMethod;
+import top.funsite.spring.action.config.ShiroConfig;
 import top.funsite.spring.action.domin.HttpErrorEntity;
 import top.funsite.spring.action.shiro.MessageConstant;
+import top.funsite.spring.action.shiro.session.BlankSession;
 import top.funsite.spring.action.util.JsonUtils;
 
 import javax.servlet.ServletRequest;
@@ -14,11 +18,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 重写关于身份验证和访问被拒绝的行为。
+ *
+ * @see org.apache.shiro.web.servlet.AbstractFilter
  */
+@Slf4j
 public class PassThruFilter extends PassThruAuthenticationFilter {
+
+    protected static final SimpleDateFormat MILLI_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     /**
      * {@inheritDoc}`lgfg
@@ -51,8 +62,29 @@ public class PassThruFilter extends PassThruAuthenticationFilter {
         if (req.getMethod().equals(RequestMethod.OPTIONS.name())) {
             return true;
         }
+
+        Session session = getSubject(request, response).getSession();
+
+        long timeout = ShiroConfig.getTimeout().getSeconds() * 1000L;
+
+        if (session != null && !BlankSession.ID.equals(session.getId()) && timeout > 0) {
+            long currentTime = System.currentTimeMillis();
+            long lastAccessTime = session.getLastAccessTime().getTime();
+
+            long diff = currentTime - lastAccessTime;
+
+            log.debug("CurrentTime   : {}", MILLI_TIME_FORMAT.format(new Date(currentTime)));
+            log.debug("LastAccessTime: {}", MILLI_TIME_FORMAT.format(session.getLastAccessTime()));
+            log.debug("Diff          : {} ms", diff);
+
+            if (diff > timeout) {
+                log.warn("timeout");
+            }
+        }
+
         return super.isAccessAllowed(request, response, mappedValue);
     }
+
 
     // org.apache.shiro.web.filter.authz.AuthorizationFilter.onAccessDenied
 

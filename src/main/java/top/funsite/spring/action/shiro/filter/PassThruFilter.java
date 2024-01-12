de1ac6811b1d8f90cd6d35f9ec68c47149adb326
kinglyq <755365600@qq.com>
2024-01-12 19:26:40 +0800

    remove

--- src/main/java/top/funsite/spring/action/shiro/filter/PassThruFilter.java
+++ src/main/java/top/funsite/spring/action/shiro/filter/PassThruFilter.java
@@ -19,7 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
 
 import static top.funsite.spring.action.domin.ServiceStatus.LOGIN_TIMEOUT;
 
@@ -30,8 +29,6 @@ import static top.funsite.spring.action.domin.ServiceStatus.LOGIN_TIMEOUT;
  */
 @Slf4j
 public class PassThruFilter extends PassThruAuthenticationFilter {
-
-    protected static final SimpleDateFormat MILLI_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     /**
      * {@inheritDoc}`lgfg

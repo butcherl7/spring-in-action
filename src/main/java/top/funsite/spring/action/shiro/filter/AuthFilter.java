d95a5e4e3d9f8b0bead71eddacac6b2a30c94d06
kinglyq <755365600@qq.com>
2024-01-12 11:03:45 +0800

    comment

--- src/main/java/top/funsite/spring/action/shiro/filter/AuthFilter.java
+++ src/main/java/top/funsite/spring/action/shiro/filter/AuthFilter.java
@@ -1,4 +1,11 @@
 package top.funsite.spring.action.shiro.filter;
 
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+/**
+ * 要求请求用户通过身份验证（已登录）才能继续请求。
+ *
+ * @see FormAuthenticationFilter
+ */
 public class AuthFilter extends PassThruFilter {
 }

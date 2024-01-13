05485444233886cbb6f529ae70f5ca0dc38e21db
kinglyq <755365600@qq.com>
2024-01-13 10:49:09 +0800

    filterClass

--- src/main/java/top/funsite/spring/action/shiro/configurers/NamedFilter.java
+++ src/main/java/top/funsite/spring/action/shiro/configurers/NamedFilter.java
@@ -1,23 +1,35 @@
 package top.funsite.spring.action.shiro.configurers;
 
+import lombok.Getter;
+import org.apache.shiro.web.filter.authc.AnonymousFilter;
 import org.apache.shiro.web.filter.mgt.DefaultFilter;
+import top.funsite.spring.action.shiro.filter.*;
+
+import javax.servlet.Filter;
 
 /**
  * 定义用到的过滤器名称。
  *
  * @see DefaultFilter
  */
+@Getter
 public enum NamedFilter {
 
-    anon,
+    anon(AnonymousFilter.class),
 
-    authc,
+    authc(AuthFilter.class),
 
-    perms,
+    perms(PermissionsFilter.class),
 
-    roles,
+    roles(RoleFilter.class),
 
-    remember,
+    remember(RememberedFilter.class),
 
-    jwt
+    jwt(JwtFilter.class);
+
+    private final Class<? extends Filter> filterClass;
+
+    NamedFilter(Class<? extends Filter> filterClass) {
+        this.filterClass = filterClass;
+    }
 }

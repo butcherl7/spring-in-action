6199a77caf9cba42bf8c49733230a5942b476458
kinglyq <755365600@qq.com>
2024-01-13 15:13:00 +0800

    DecodedJWTValidator

---
+++ src/main/java/top/funsite/spring/action/shiro/filter/DecodedJWTValidator.java
@@ -0,0 +1,25 @@
+package top.funsite.spring.action.shiro.filter;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 对已经正确解码过的 JWT 进行进一步的验证。
+ *
+ * @see JwtFilter
+ */
+@FunctionalInterface
+public interface DecodedJWTValidator {
+
+    /**
+     * 验证解码过的 JWT 令牌。
+     *
+     * @param request    the incoming HttpServletRequest
+     * @param response   the outgoing HttpServletResponse
+     * @param decodedJWT 解码的 JSON Web Token.
+     * @return 如果请求应继续处理，则为 true，如果子类将直接处理/呈现响应，则为 false。
+     */
+    boolean validate(HttpServletRequest request, HttpServletResponse response, DecodedJWT decodedJWT);
+}

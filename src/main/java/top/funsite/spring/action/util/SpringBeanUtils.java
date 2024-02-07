a6e70c5da237d0962422e150225779ad9ec992ef
kinglyq <755365600@qq.com>
2024-02-07 09:40:56 +0800

    listen to suggest

--- src/main/java/top/funsite/spring/action/util/SpringBeanUtils.java
+++ src/main/java/top/funsite/spring/action/util/SpringBeanUtils.java
@@ -1,6 +1,7 @@
 package top.funsite.spring.action.util;
 
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -15,7 +16,7 @@ public class SpringBeanUtils implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
         SpringBeanUtils.applicationContext = applicationContext;
     }
 

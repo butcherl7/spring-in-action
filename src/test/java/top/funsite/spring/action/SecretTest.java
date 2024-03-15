c88eeb85853e2ab92235a0bc8a425fc17180dfa9
kinglyq <755365600@qq.com>
2024-03-15 19:04:37 +0800

    shiro BCrypt test

--- src/test/java/top/funsite/spring/action/SecretTest.java
+++ src/test/java/top/funsite/spring/action/SecretTest.java
@@ -10,6 +10,13 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.credential.DefaultPasswordService;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.crypto.hash.DefaultHashService;
+import org.apache.shiro.crypto.hash.Hash;
+import org.apache.shiro.crypto.hash.HashProvider;
+import org.apache.shiro.crypto.hash.HashSpi;
+import org.apache.shiro.lang.util.SimpleByteSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,8 +24,8 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 public class SecretTest {
@@ -98,4 +105,42 @@ public class SecretTest {
         System.out.println(verify.verified);
     }
 
+    @Test
+    public void testBCryptHash() {
+        HashedCredentialsMatcher credentialsMatcher = new HashedCredentialsMatcher("2b");
+        credentialsMatcher.setHashIterations(4);
+
+        // SimpleHash simpleHash = new SimpleHash("bcrypt", "123456", SimpleByteSource.empty(), 3);
+        // System.out.println(hash);
+
+        List<String> hashAlgorithms = StreamSupport.stream(ServiceLoader.load(HashSpi.class).spliterator(), false)
+                .map(HashSpi::getImplementedAlgorithms)
+                .flatMap(Set::stream)
+                .toList();
+        System.out.println("HashAlgorithms: " + hashAlgorithms);
+
+        // $2y$10$7rOjsAf2U/AKKqpMpCIn6etuOXyQ86tp2Tn9xv6FyXl2T0QYc3.G.
+        String TEST_PASSWORD = "secret#shiro,password;Jo8opech";
+
+        String plainText = "123456";
+
+        DefaultHashService hashService = new DefaultHashService();
+        hashService.setDefaultAlgorithmName("2b");
+
+        DefaultPasswordService passwordService = new DefaultPasswordService();
+        passwordService.setHashService(hashService);
+
+        String encryptPassword = passwordService.encryptPassword(plainText);
+        System.out.println(encryptPassword);
+
+        boolean b1 = passwordService.passwordsMatch(plainText, encryptPassword);
+        System.out.println(b1);
+
+        HashProvider.getByAlgorithmName("2b").ifPresent(hashSpi -> {
+            Hash hash = hashSpi.fromString("$2b$10$kJ/Gvbcc1bXD929k5DXA..TAs5sfvHNddDU1jaqy2GvH9aAEhybp.");
+            boolean b = hash.matchesPassword(new SimpleByteSource(plainText));
+            System.out.println(b);
+            System.out.println(hash);
+        });
+    }
 }

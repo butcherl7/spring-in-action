d68e68e867410e2b6f5f0d6b5b39ef7077a58d02
kinglyq <755365600@qq.com>
2024-03-19 14:17:32 +0800

    PasswordMatcher

--- src/main/java/top/funsite/spring/action/shiro/credential/BcryptPasswordMatcher.java
+++ src/main/java/top/funsite/spring/action/shiro/credential/BcryptPasswordMatcher.java
@@ -1,21 +1,13 @@
 package top.funsite.spring.action.shiro.credential;
 
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
+import org.apache.shiro.authc.credential.PasswordMatcher;
 
 /**
  * 使用 Bcrypt 加密算法验证传入的密码是否与系统中存储的相应帐户的密码匹配。
  */
-public class BcryptPasswordMatcher extends SimpleCredentialsMatcher {
+public class BcryptPasswordMatcher extends PasswordMatcher {
 
-    private static final StrongerHashPasswordService PASSWORD_SERVICE = new StrongerHashPasswordService();
-
-    @Override
-    public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
-        String submittedPassword = toString(token.getCredentials());
-        String storedCredentials = toString(info.getCredentials());
-
-        return PASSWORD_SERVICE.passwordsMatch(submittedPassword, storedCredentials);
+    public BcryptPasswordMatcher() {
+        setPasswordService(new StrongerHashPasswordService());
     }
 }

3b731e2e68d310ccd8539c504712bd8813827367
kinglyq <755365600@qq.com>
2024-04-04 16:02:28 +0800

    2>1

--- src/test/java/top/funsite/spring/action/SpringInActionApplicationTests.java
+++ src/test/java/top/funsite/spring/action/SpringInActionApplicationTests.java
@@ -1,13 +1,45 @@
 package top.funsite.spring.action;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import top.funsite.spring.action.domain.Dolly;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 @SpringBootTest
 class SpringInActionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testRedisSet() {
+        Dolly dolly = new Dolly();
+        dolly.setName("Dolly");
+        dolly.setSize(25);
+        dolly.setGender(false);
+        dolly.setDate(new Date());
+        dolly.setLocalTime(LocalTime.now());
+        dolly.setLocalDate(LocalDate.now());
+        dolly.setDateTime(LocalDateTime.now());
+        redisTemplate.opsForValue().set("dolly", dolly);
+    }
+
+    @Test
+    public void testRedisGet() {
+        Object dolly = redisTemplate.opsForValue().get("dolly");
+        assertInstanceOf(Dolly.class, dolly);
+    }
 
 }

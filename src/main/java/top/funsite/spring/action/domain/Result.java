5ecbb44d8a34dae642ea06083765293c8517e6a0
Butcher <755365600@qq.com>
2024-05-18 16:14:18 +0800

    aj

--- src/main/java/top/funsite/spring/action/domain/Result.java
+++ src/main/java/top/funsite/spring/action/domain/Result.java
@@ -1,9 +1,13 @@
 package top.funsite.spring.action.domain;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 /**
- * Result 是一个泛型记录类，用于表示某个接口调用的结果。
+ * Result 是一个泛型记录类，作为接口调用响应的结果。
  *
- * @param error     表示业务是否出错
+ * @param error     {@code True}表示调用出现错误
  * @param message   提示信息
  * @param data      返回的数据
  * @param timestamp 响应的时间
@@ -12,6 +16,8 @@ public record Result<T>(boolean error, String message, T data, long timestamp) {
 
     // @JsonInclude(JsonInclude.Include.NON_NULL) // null 不序列化
 
+    private static final String OK = "OK";
+
     /**
      * 响应业务成功。
      *
@@ -19,16 +25,17 @@ public record Result<T>(boolean error, String message, T data, long timestamp) {
      * @return OK Result with data.
      */
     public static <T> Result<T> ok(T data) {
-        return new Result<>(false, "OK", data);
+        return new Result<>(false, OK, data);
     }
 
     /**
      * 响应业务失败。
      *
-     * @param message msg，错误消息。
-     * @return failed Result.
+     * @param message 业务失败的描述信息（不能为空）。
+     * @return Result.
      */
-    public static Result<Void> fail(String message) {
+    public static Result<Void> fail(@NotNull String message) {
+        Objects.requireNonNull(message, "message must not be null");
         return new Result<>(true, message, null);
     }
 

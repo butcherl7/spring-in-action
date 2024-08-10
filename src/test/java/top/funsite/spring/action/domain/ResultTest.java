0b51de29ada56ea28c88817982e92e887119d7d2
Butcher <755365600@qq.com>
2024-08-10 11:39:51 +0800

    ResultTest

---
+++ src/test/java/top/funsite/spring/action/domain/ResultTest.java
@@ -0,0 +1,32 @@
+package top.funsite.spring.action.domain;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.junit.jupiter.api.Test;
+import top.funsite.spring.action.domain.r.Pagination;
+import top.funsite.spring.action.domain.r.Result;
+import top.funsite.spring.action.util.JsonUtils;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class ResultTest {
+
+    @Test
+    public void testOkPagination() throws JsonProcessingException {
+        Map<String, String> data = Collections.emptyMap();
+        Pagination<String> pagination = new Pagination<>(0, Collections.emptyList());
+
+        var result1 = Result.ok(data);
+        var result2 = Result.ok(pagination);
+
+        ObjectWriter objectWriter = JsonUtils.OBJECT_MAPPER.writerWithDefaultPrettyPrinter();
+
+        var json1 = objectWriter.writeValueAsString(result1);
+        var json2 = objectWriter.writeValueAsString(result2);
+
+        System.out.println(json1);
+        System.out.println("*******");
+        System.out.println(json2);
+    }
+}
